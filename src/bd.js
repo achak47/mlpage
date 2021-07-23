caffee911754d06979e1f0b0d67efbd8c6fd5346
Your Name <you@example.com>
2021-07-23 10:26:00 +0530

    Date Change

--- src/bd.js
+++ src/bd.js
@@ -5,7 +5,7 @@ const Body = ({onRouteChange})=>{
    <div className='container'>
        <p class='c1'>IEEE JU Presents</p>
        <p className='c2'>MLAS 2020</p>
-       <p className='c3'>(Machine Learning Accelerator Summit)<br/>19-20 April 2020 , Jadavpur University WB .</p>
+       <p className='c3'>(Machine Learning Accelerator Summit)<br/>20 October 2020 , Jadavpur University WB .</p>
        <button type="button" class="btn btn-primary btn-lg" onClick={()=>onRouteChange('Register')}>REGISTER</button>
    </div>
   );
